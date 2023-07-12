96e9c9551c037f061a2b4ed3a78d62ace1041bd3
unknown <kiarie7mbugua@gmail.com>
2023-07-12 19:45:19 +0300

    cl

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <head>
+        <h1>Hello world</h1>
+      </head>
+    </>
+  );
+}
+
+export default App;
